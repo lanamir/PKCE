bee17bc28aa05da5e6921452f5ca7ff27d1c4b26
unknown <enjoy.the.silence.inside@gmail.com>
2023-07-12 13:06:08 +0300

    moved some files

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/*
+let hello = document.getElementById('hello');
+hello.addEventListener('click', () => {
+    console.log('Hello');
+});
+
+let goodbye = document.getElementById('goodbye');
+goodbye.addEventListener('click', () => {
+    console.log('Goodbye');
+});
+*/
+
+
+
+/*
+//Button
+const btn = document.querySelector('.btn-primary');
+const remove = document.querySelector('.btn-warning');
+
+const buttonClickHandler = function(event) {
+    event.target.disabled = true;
+    console.log(event);
+}
+
+btn.addEventListener('click', buttonClickHandler);*/
+
+/*
+const bindFunc = buttonClickHandler.bind(this);
+
+btn.addEventListener('click', bindFunc);
+remove.addEventListener('click', () => {
+    btn.removeEventListener('click', bindFunc);
+});
+
+
+btn.onclick = function func_name(argument) {
+    //body...
+}
+*/
+
